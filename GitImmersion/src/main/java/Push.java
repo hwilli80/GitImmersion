24d7c4b6b72a1303ac5429e933a9c84e6b5f1f4b
Howard Williams <hwilli80@uncc.edu>
2020-11-02 22:13:07 -0500

    Goodbye

--- GitImmersion/src/main/java/Push.java
+++ GitImmersion/src/main/java/Push.java
@@ -5,6 +5,7 @@ class MyFirstJavaProgram {
     */
 
    public static void main(String []args) {
-      System.out.println("Hello World"); // prints Hello World
+      System.out.println("GoodBye World"); // prints Hello World
+      
    }
 }
